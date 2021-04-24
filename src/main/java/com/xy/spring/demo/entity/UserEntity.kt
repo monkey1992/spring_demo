771a9bef030bd46839ab5cf79b87926c0af5fa1c
xiaoyu <yuriyshea@163.com>
2021-04-24 23:10:09 +0800

    通过JsonIgnoreProperties忽略敏感信息字段

--- src/main/java/com/xy/spring/demo/entity/UserEntity.kt
+++ src/main/java/com/xy/spring/demo/entity/UserEntity.kt
@@ -1,8 +1,10 @@
 package com.xy.spring.demo.entity
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(value = ["pwd"])
 data class UserEntity(
 
         /** 用户ID  */
